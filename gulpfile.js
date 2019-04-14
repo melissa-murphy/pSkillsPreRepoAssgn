5006da8dc7f1bb11b3788dbd72bb7ab23102b8d2
Melissa Murphy <melissa@thoughtfulmonkey.design>
2019-04-14 01:30:18 -0700

    scaffolding done

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin'),
+    cache = require('gulp-cache');
+var sass = require('gulp-sass');
+
+gulp.task('images', function(){
+  gulp.src('src/images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/images/'));
+});
+
+gulp.task('styles', function(){
+  gulp.src(['src/scss/**/*.scss'])
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(sass())
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(gulp.dest('dist/scss/'))
+});
+
+gulp.task('scripts', function(){
+  return gulp.src('src/scripts/**/*.js')
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('main.js'))
+    .pipe(babel())
+    .pipe(gulp.dest('dist/scripts/'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts/'))
+});
+
+gulp.task('default', function(){
+  gulp.watch("src/scss/**/*.scss", ['styles']);
+  gulp.watch("src/scripts/**/*.js", ['scripts']);
+});
